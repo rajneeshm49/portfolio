93c3289e0b3c2123dbeed14a04890e223bd4e1d4
Rajneesh Mishra <rajneesh.m49@gmail.com>
2024-10-12 12:54:35 +0530

    add sample projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,16 @@
 import React from "react";
-import PROJECTS from "../data/projects";
+import { PROJECTS, SIDE_PROJECTS } from "../data/projects";
 import Project from "./Project";
 
 const Projects = () => (
   <div>
     <h2>Highlighted Projects</h2>
     {PROJECTS.map((PROJECT) => (
+      <Project project={PROJECT} />
+    ))}
+    <hr />
+    <h2>Side Projects</h2>
+    {SIDE_PROJECTS.map((PROJECT) => (
       <Project project={PROJECT} />
     ))}
   </div>
